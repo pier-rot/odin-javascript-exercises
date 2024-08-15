32de68aa15e57a65d1f1232387038acb72452856
Pierre V <6772696d@gmail.com>
2024-08-15 09:12:30 +0200

    Complete '05_sumAll'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	let error = "ERROR";
+	if (start < 0 || end < 0) {
+		return error;
+	} else if (!(typeof start === "number") || typeof end !== "number") {
+		return error;
+	} else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+		return error;
+	}
 
+	let sum = 0;
+	if (start < end) {
+		for (let i = start; i <= end; i++) {
+			sum += i;
+		}
+	} else {
+		for (let i = end; i <= start; i++) {
+			sum += i;
+		}
+	}
+	return sum;
 };
 
 // Do not edit below this line
