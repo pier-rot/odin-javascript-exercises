3bfc5208ae727dfaa24d62cc195cd73d231d6ec0
Pierre V <6772696d@gmail.com>
2024-08-15 18:38:10 +0200

    Complete '09_palindromes'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let filteredString = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{1,}/g,"").toLowerCase();
+    let reverseString = "";
+    for (let i = 0; i < filteredString.length; i++) {
+        reverseString += filteredString[filteredString.length - 1 -i];
+    }
 
+    console.log(filteredString);
+    console.log(reverseString);
+    if (reverseString == filteredString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
