0bc99749b531ddc53495dacc84531173b57dbadc
Pierre V <6772696d@gmail.com>
2024-08-15 18:26:48 +0200

    Complete '08_calculator'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,44 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((acc, current) => {
+		return acc + current;
+	}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((acc, current) => {
+		return acc * current;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	function factAux(n, acc) {
+		if (n == 0) {
+			return acc;
+		} else {
+			return factAux(n - 1, n * acc);
+		}
+	}
+	return factAux(n,1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
