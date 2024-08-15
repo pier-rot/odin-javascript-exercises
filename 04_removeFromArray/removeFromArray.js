93637e92172cc6543a778328cc0d76f4121ec633
Pierre V <6772696d@gmail.com>
2024-08-15 08:58:44 +0200

    Complete '04_removefromarray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+  let arr = array;
+  for (let i = 0; i < elements.length; i++) {
+    arr = arr.filter(
+      function (elem) {
+        return elem !== elements[i];
+      }
+    )
+  }
+  console.log(arr);
+  return arr;
 };
 
 // Do not edit below this line
